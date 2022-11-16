87661cf0787c9726b7ab5dfc33adc03aa4cba380
Liz Roberta <108678335+lizandriz@users.noreply.github.com>
2022-11-16 16:32:09 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+const form = document.querySelector('form')
+const inputWeight = document.querySelector ('#weight')
+const inputHeight = document.querySelector ('#height')
+
+const modalWrapper = document.querySelector('.modal-wrapper')
+const modalMessage = document.querySelector('.modal .title span')
+const modalBtClose = document.querySelector('.modal button.close')
+
+
+form.onsubmit = function(event) {
+    event.preventDefault()
+    
+    const weight = inputWeight.value
+    const height = inputHeight.value
+    
+    const result = IMC(weight, height)
+    const message = `Seu IMC é de ${result}`
+    
+    modalMessage.innerText = message
+    modalWrapper.classList.add('open')
+
+}
+
+modalBtClose.onclick = () => {
+    modalWrapper.classList.remove('open')
+}
+
+function IMC(weight,height){
+    return(weight/ ((height/100) **2)).toFixed(2)
+}
+
+window.addEventListener('keydown', handleKeydown)
+function handleKeydown(event){
+    if(event.key ==='Escape') {
+        console.log('close')
+    }
+}
